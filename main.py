7210ab930c9f14d43e6f79df9b565734b8976abe
skunkr <31898648+skunkr@users.noreply.github.com>
2022-09-20 12:06:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -25,4 +25,4 @@ def mainf():
 	return render_template("index.html", textres = textres, baseres = baseres)
 
 
-app.run(host="0.0.0.0", port=8080, debug=True)
+app.run(host="0.0.0.0", port=80, debug=True)
